a1f70e988f56c0672d5e01ba7432c44071017cc7
Bradley <bmwise14@gmail.com>
2024-10-30 22:41:46 -0400

    first commit on langgraph agent test

---
+++ src/agent.py
@@ -0,0 +1,222 @@
+from dotenv import load_dotenv
+_ = load_dotenv()
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated, List, Dict, Any
+import operator
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage, AIMessage, ChatMessage
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
+from pydantic import BaseModel, Field
+from langchain_core.tools import BaseTool
+
+import requests
+import os
+# import pyodbc
+from uuid import uuid4
+import ast
+import prompts
+
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+#############################################################
+def reduce_messages(left: list[AnyMessage], right: list[AnyMessage]) -> list[AnyMessage]:
+    # assign ids to messages that don't have them
+    for message in right:
+        if not message.id:
+            message.id = str(uuid4())
+    # merge the new messages with the existing messages
+    merged = left.copy()
+    for message in right:
+        for i, existing in enumerate(merged):
+            # replace any existing messages with the same id
+            if existing.id == message.id:
+                merged[i] = message
+                break
+        else:
+            # append any new messages to the end
+            merged.append(message)
+    return merged
+
+#############################################################
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], reduce_messages]
+    summary: str
+    last_human_index : int
+    title: str
+    
+class Agent:
+    def __init__(self, model, tools, checkpointer, temperature=0.1, system=""):
+        self.system = system
+        self.temperature=temperature
+        self.tools = {t.name: t for t in tools} if tools else {}
+        self.model = model.bind_tools(tools) if tools else model
+        
+        graph = StateGraph(AgentState)
+        graph.add_node("process_input", self.process_input)
+        graph.add_node("llm", self.call_openai)
+        graph.add_node("summarize", self.summarize_conversation)
+
+        graph.add_edge("process_input", "llm")
+        
+        if tools:
+            graph.add_node("action", self.take_action)
+            graph.add_conditional_edges(
+              "llm", ## where the node starts
+              self.exists_action, ## function to determine where to go after LLM executes
+              {True: "action", False : "summarize"} # map response to the function to next node to go to 
+            )
+            graph.add_edge("action", "llm") # send action to llm
+        else:
+            graph.add_edge('llm', 'summarize')
+            
+        graph.add_edge("summarize", END)
+        
+        graph.set_entry_point("process_input") ## "llm"
+        self.graph = graph.compile(checkpointer=checkpointer)
+
+    def process_input(self, state: AgentState):
+        messages = state.get('messages', [])
+        # print("MESSAGES")
+        # print(messages)
+        last_human_index = len(messages) - 1
+        for i in reversed(range(len(messages))):
+            if isinstance(messages[i], HumanMessage):
+                last_human_index = i
+                break
+                
+        # Check if a title already exists
+        if not state.get('title'):
+            # Find the first human message
+            first_human_message = next((msg for msg in messages if isinstance(msg, HumanMessage)), None)
+            
+            if first_human_message:
+                # Create a title based on the first human message
+                title_query = [
+                    SystemMessage(content="Make a short, concise title based on the following query:"),
+                    first_human_message
+                ]
+                title_response = self.model.invoke(title_query, temperature=0.01)
+                title = title_response.content
+            else:
+                title = "New Conversation"
+        else:
+            # If a title already exists, keep it
+            title = state['title']
+        
+        return {"last_human_index": last_human_index, "title" : title}
+
+    def get_relevant_messages(self, state: AgentState) -> List[AnyMessage]:
+        '''
+        Don't get tool call messages for AI from history.
+        Get state from everything up to the most recent human message
+        '''
+        messages = state['messages']
+        filtered_history = []
+        for message in messages:
+            if isinstance(message, HumanMessage) and message.content!="":
+                filtered_history.append(message)
+            elif isinstance(message, AIMessage) and message.content!="" and message.response_metadata['finish_reason']=="stop":
+                filtered_history.append(message)
+        last_human_index = state['last_human_index']
+        return filtered_history[:-1] + messages[last_human_index:]
+        
+    def summarize_conversation(self, state: AgentState):
+        messages = state['messages']
+        existing_summary = state.get("summary", "")
+
+        if existing_summary:
+            summary_prompt = (
+                f"""This is a summary of the content of the conversation to date: {existing_summary}
+                Extend the summary by taking into account any unseen messages above.
+                Prioritize gathering facts of the conversation that you can reference later for long-term memory:"""
+            )
+        else:
+            summary_prompt = (
+                "Create a summary of the content of the conversation above. "
+                "Prioritize gathering facts of the conversation that you can reference later for long-term memory:"
+            )
+
+        summary_messages = messages + [HumanMessage(content=summary_prompt)]
+
+        try:
+            summary_response = self.model.invoke(summary_messages, temperature=0.01)
+        except Exception as e:
+            summary_messages = [HumanMessage(content="Too much context from all the messages before")] + [HumanMessage(content=summary_prompt)]
+            # print(summary_messages)
+            summary_response = self.model.invoke(summary_messages, temperature=0.01)
+        
+        return {
+            "summary": summary_response.content,
+            "messages": [messages[-1]],  # Only keep the last message
+            "last_human_index": 0
+        }
+    
+    def call_openai(self, state: AgentState):
+        '''All nodes and edges will take this in'''
+        relevant_messages = self.get_relevant_messages(state) # get Human/AI history and only all other messages since the last human message
+        
+        messages = [SystemMessage(content=self.system)] + relevant_messages
+        try:
+            print("TRY MODEL INVOCATION")
+            response = self.model.invoke(messages, temperature=self.temperature)
+            print(response)
+            return {"messages" : [response]}
+        except Exception as e:
+            print("CONTEXT TOO BIG")
+            finality_message = [HumanMessage(content="Please Reply: 'I retrieved too much information in my searches and couldn't fit it in my context window. Try again or try tweaking your question to make the search space smaller. I apologize for the inconvenience.'")]
+            response = self.model.invoke(finality_message, temperature=0.01)
+            return {"messages" : [response]}
+
+    def take_action(self, state: AgentState):
+        ''' Get last message from agent state.
+        If we get to this state, the language model wanted to use a tool.
+        The tool calls attribute will be attached to message in the Agent State. Can be a list of tool calls.
+        Find relevant tool and invoke it, passing in the arguments
+        '''
+        last_message = state["messages"][-1]
+        if not isinstance(last_message, AnyMessage) or not last_message.tool_calls: # AIMessage
+            return {"messages": state['messages']}
+            
+        results = []
+        for t in last_message.tool_calls:
+            print(f'Calling: {t}')
+
+            if not t['name'] in self.tools: # check for bad tool name
+                print("\n ....bad tool name....")
+                result = "bad tool name, retry" # instruct llm to retry if bad
+            else:
+                # pass in arguments for tool call
+                result = self.tools[t['name']].invoke(t['args'])
+
+            # append result as a tool message
+            results.append(ToolMessage(tool_call_id = t['id'], name=t['name'], content=str(result)))
+
+        return {"messages" : results} # langgraph adding to state in between iterations
+    
+    def exists_action(self, state:AgentState):
+        '''
+        takes in result after the LLM is called and return True/False if no tool calls vs calls exist. 
+        Should we take an action or should we not?
+        '''
+        if not self.tools:
+            return False
+        
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+    
+if __name__=="__main__":
+    prompt = prompts.agent_prompt
+    temperature=0.1
+    checkpointer = MemorySaver()
+    model=ChatOpenAI(model='gpt-4o-mini')
+    thread_id = "test"
+
+    ##############
+    agent = Agent(model, [], checkpointer=checkpointer, temperature=temperature, system=prompt)
+    agent_input = {"messages" : [HumanMessage(content="Hi, my name is Brad")]}
+    thread_config = {"configurable" : {"thread_id" : thread_id}}
+    result = agent.graph.invoke(agent_input, thread_config)
+    print(result['messages'][-1].content)
+
+    # print(agent.graph.get_graph().print_ascii())
---
+++ src/prompts.py
@@ -0,0 +1,7 @@
+agent_prompt = '''You are an agent that is looking up information at a software company. 
+Use the tools at your dispoal to look up and verify information. You are allowed to make multiple calls (either together or in sequence).
+Only look up information when you are sure of what you want. If you need to look up some information before asking a follow-up question,
+you are allowed to do that.
+
+If there are no tools available, try to answer the question to the best of your ability.
+'''
